56825a425c69523f9fb6647027f2bc31de09224a
ferhatsusam <dev.fredx@gmail.com>
2024-02-08 20:57:06 +0300

    Explore the simple structure of python: -syntax, comment lines, variables and data structures

---
+++ week1/intro/main.py
@@ -0,0 +1,59 @@
+print("Hello!")
+
+#Comment line
+
+x = 5
+y = "John"
+print(x)
+print(y)
+
+print(type(y))
+
+a = str(3)
+b = int(3)
+c = float(3)
+
+print(a,b,c)
+
+d, e, f = 1, 2, 3
+
+print(d,e,f)
+
+e = 10
+print(e)
+
+D = 10
+print(D)
+
+i = None
+
+print(i)
+
+X = 5
+print(X)
+X = "Hello"
+print(X)
+
+x = "h"
+
+print("\n")
+
+a = 5
+b = "Hello World"
+c = 20.5
+d = 1j
+e = ["apple", "banana", "cherry"]
+f = ("apple", "banana", "cherry")
+g = range(6)
+h = True
+i = None
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
+print(type(i))
+

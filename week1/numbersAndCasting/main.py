4eb388e119ce8809d043c7a0eaf737656a271eeb
ferhatsusam <dev.fredx@gmail.com>
2024-02-09 22:32:40 +0300

    explore: numbers, type conversion, casting

---
+++ week1/numbersAndCasting/main.py
@@ -0,0 +1,58 @@
+x = 35656222554887711
+print(x)
+print(type(x))
+
+y = 22.19
+z = -87.7e100
+print(y, z)
+print(type(x))
+print(type(z))
+
+x = 12e4
+print(x)
+print(type(x))
+
+y = 5j
+print(y)
+print(type(y))
+
+a = 1
+d = float(a)
+print(d)
+
+b = 12.1
+e = int(b)
+print(e)
+
+c = 4
+f = complex(c)
+print(f)
+
+import random
+
+print(random.randrange(4, 73))
+
+
+x = int(21)
+y = int(31.8)
+z = int("0")
+print(x)
+print(y)
+print(z)
+
+x = float(3)
+y = float(11.8)
+z = float("6")
+w = float("4.7")
+print(x)
+print(y)
+print(z)
+print(w)
+
+x = str("s5")
+y = str(8)
+z = str(1.0)
+print(x)
+print(y)
+print(z)
+

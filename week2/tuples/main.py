570f36e6a879ea60ac5c1755f132847af76a9830
ferhatsusam <dev.fredx@gmail.com>
2024-02-13 21:50:18 +0300

    explore the structure and functionality of tuples

--- week2/tuples/main.py
+++ week2/tuples/main.py
@@ -1,16 +1,77 @@
-# This is a sample Python script.
+# unchangeable
+myTuple = (10, "a", 8.3, True, 9, "x")
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+print(len(myTuple))
+print(type(myTuple))
+
+tuple1 = ("python",)
+print(type(tuple1))
+
+# not a tuple
+tuple2 = ("python")
+print(type(tuple2))
+
+tuple3 = tuple(("python", "java", "ruby"))
+print(type(tuple3))
+
+print(myTuple[1])
+print(myTuple[-2])
+print(myTuple[1:3])
+print(myTuple[:4])
+print(myTuple[2:])
+print(myTuple[-3:-2])
+
+if "a" in myTuple:
+    print("Yes")
+print()
+
+# myTuple[1] = 200
+# print(myTuple)
+
+x = list(myTuple)
+x[1] = "200"
+myTuple = tuple(x)
+print(x)
+print()
+
+x = list(myTuple)
+x.append(False)
+myTuple = tuple(x)
+print(x)
+print()
+
+x = ("123",)
+myTuple += x
+print(myTuple)
+print()
+
+x = list(myTuple)
+x.remove("123")
+myTuple = tuple(x)
+print(myTuple)
+print()
+
+# unpacking
+(python, java, ruby) = tuple3
+print(python)
+print(java)
+print(ruby)
+print()
+
+totalTuple = myTuple + tuple3
+print(totalTuple)
+
+tuple4 = totalTuple * 2
+print(tuple4)
+print()
+
+# methods
+numberTuple = (1,2,3,4,5,6,7,8,5)
+x = numberTuple.count(5)
+print(x)
+
+x = numberTuple.index(8)
+print(x)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/

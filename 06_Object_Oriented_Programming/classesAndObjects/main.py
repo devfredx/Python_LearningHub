4807657a631fb2d15e3190ce9692e098a7965d87
devfredx <dev.fredx@gmail.com>
2026-01-10 21:27:05 +0300

    feat: add basic class definitions and object instantiation examples

--- 06_Object_Oriented_Programming/classesAndObjects/main.py
+++ 06_Object_Oriented_Programming/classesAndObjects/main.py
@@ -1,18 +1,52 @@
-class Person():
-    name = ""
-    age = 0
-    gender = ""
+class Player:
+    def __init__(self, username, level):
+        self.username = username
+        self.level = level
 
-    #method, initializer
-    def __init__(self):
-        print("init executed")
+    def show_info(self):
+        print(f"Player: {self.username} | Level: {self.level}")
 
-myList = list()
-print(type(myList))
+    def level_up(self):
+        self.level += 1
+        print(f"{self.username} reached level {self.level}!")
 
-fred = Person()
-print(type(fred))
 
-fred.name = "fred"
-fred.age = 18
-print(fred.name)
+player1 = Player("Agent_47", 10)
+player2 = Player("Neo", 25)
+
+print(player1.username)
+print(player2.level)
+
+player1.show_info()
+player2.show_info()
+
+player1.level_up()
+
+
+class Weapon:
+    damage = 50
+
+    def __init__(self, name):
+        self.name = name
+
+    def fire(self):
+        print(f"{self.name} is firing! Damage: {self.damage}")
+
+
+pistol = Weapon("Glock")
+pistol.fire()
+
+print(isinstance(player1, Player))
+print(hasattr(player2, "level"))
+
+setattr(player1, "rank", "Elite")
+print(getattr(player1, "rank"))
+
+delattr(player1, "rank")
+
+
+class EmptyClass:
+    pass
+
+
+empty_obj = EmptyClass()

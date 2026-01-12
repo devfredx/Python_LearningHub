2a7bd83ec6e4c00453d46500430ea3ef045be012
devfredx <dev.fredx@gmail.com>
2026-01-12 20:22:15 +0300

    feat: add math module constants and functions for advanced calculations

---
+++ 07_Advanced_and_Libraries/math_operations/main.py
@@ -0,0 +1,42 @@
+import math
+
+print(math.pi)
+print(math.e)
+print(math.tau)
+
+print(math.ceil(4.2))
+print(math.floor(4.8))
+print(math.trunc(4.9))
+
+print(math.sqrt(64))
+print(math.isqrt(65))
+print(math.pow(2, 3))
+
+print(math.fabs(-10.5))
+print(math.factorial(5))
+
+print(math.log(100, 10))
+print(math.log2(8))
+print(math.log10(1000))
+
+print(math.sin(math.radians(90)))
+print(math.cos(0))
+print(math.tan(math.radians(45)))
+
+print(math.degrees(math.pi))
+print(math.radians(180))
+
+print(math.gcd(12, 18))
+print(math.lcm(4, 6))
+
+print(math.isclose(0.1 + 0.2, 0.3))
+print(math.isfinite(100))
+print(math.isinf(math.inf))
+print(math.isnan(math.nan))
+
+points = [1, 2, 3, 4]
+print(math.fsum(points))
+
+print(math.prod([2, 3, 4]))
+
+print(math.remainder(10, 3))

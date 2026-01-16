c0ddfeaa05039d4469d088abba417a5c062993a7
devfredx <dev.fredx@gmail.com>
2026-01-16 20:42:22 +0300

    feat: add entry creation logic with timestamp and mood support

--- 09_Simple_Projects/daily_journal/main.py
+++ 09_Simple_Projects/daily_journal/main.py
@@ -4,3 +4,12 @@ from datetime import datetime
 class Journal:
     def __init__(self):
         self.entries = []
+
+        def add_entry(self, text, mood="Neutral"):
+            entry = {
+                "date": datetime.now().strftime("%Y-%m-%d %H:%M"),
+                "content": text,
+                "mood": mood
+            }
+            self.entries.append(entry)
+            print("Entry added successfully!")append(entry)

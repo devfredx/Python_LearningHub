4d7e689e4575cd0391a6be2f013e84e3839fd879
devfredx <dev.fredx@gmail.com>
2026-01-18 10:43:22 +0300

    feat: add JSON storage functionality for data persistence

--- 09_Simple_Projects/budgetTracker/tracker.py
+++ 09_Simple_Projects/budgetTracker/tracker.py
@@ -1,3 +1,6 @@
+import json
+import os
+
 class BudgetTracker:
     def __init__(self):
         self.transactions = []
@@ -10,3 +13,12 @@ class BudgetTracker:
         total_income = sum(t['amount'] for t in self.transactions if t['type'] == "Income")
         total_expense = sum(t['amount'] for t in self.transactions if t['type'] == "Expense")
         return total_income - total_expense
+
+    def save_data(self, filename="data.json"):
+        with open(filename, "w") as f:
+            json.dump(self.transactions, f, indent=4)
+
+    def load_data(self, filename="data.json"):
+        if os.path.exists(filename):
+            with open(filename, "r") as f:
+                self.transactions = json.load(f)
